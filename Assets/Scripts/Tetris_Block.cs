693723a829da24e3a2dca1051be8465091712f57
Layal Singh <layal.singhkolkata@gmail.com>
2025-01-22 13:53:12 -0500

    Rigidbody2D, colliders adjustments

--- Assets/Scripts/Tetris_Block.cs
+++ Assets/Scripts/Tetris_Block.cs
@@ -1,68 +1,71 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+//import something for collision detection
 
 public class Tetris_Block : MonoBehaviour
 {
     public Vector3 rotationPoint;
-    float fallTime = 0.8f;
-    float previousTime;
     public static int height = 20;
     public static int width = 12;
-
+    private Rigidbody2D rb;
+    private bool collided = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Horizontal movement
+        if (!collided)
+        { 
+            OnInput();
+        }
+       
+    }
+
+   void OnInput()
+    {
         if ((Input.GetKeyDown(KeyCode.LeftArrow)) || (Input.GetKeyDown(KeyCode.A)))
         {
             transform.position += new Vector3(-1, 0, 0);
-            if (!isValidGridPos())           
+            if (!isValidGridPos())
             {
                 transform.position += new Vector3(1, 0, 0);
             }
-            
-         
         }
+
         else if ((Input.GetKeyDown(KeyCode.RightArrow)) || (Input.GetKeyDown(KeyCode.D)))
         {
-           transform.position += new Vector3(1, 0, 0);
+            transform.position += new Vector3(1, 0, 0);
             if (!isValidGridPos())
             {
                 transform.position += new Vector3(-1, 0, 0);
             }
-            
+
         }
         else if ((Input.GetKeyDown(KeyCode.UpArrow)) || (Input.GetKeyDown(KeyCode.W)))
         {
-            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0,0,1), 90);
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
             if (!isValidGridPos())
             {
-                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0,0,1), -90);
-            }
-        }
-
-        // Vertical movement
-        if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
-        {
-            transform.position += new Vector3(0, -1, 0);
-            previousTime = Time.time;
-            if (!isValidGridPos())
-            {
-                transform.position += new Vector3(0, 1, 0);
-                this.enabled = false;
-                FindObjectOfType<SpawnBlock>().NewBlock();
-                
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
             }
         }
     }
 
+    void  OnCollisionEnter2D (Collision2D collision)
+    {
+        if (collided) return;
+        collided = true;
+        
+        rb.linearVelocity = Vector2.zero;
+        rb.angularVelocity = 0;
+        FindObjectOfType<SpawnBlock>().NewBlock();
+    }
     // Check if the block is within the bounds of the grid
     bool isValidGridPos()
     {
@@ -79,4 +82,5 @@ public class Tetris_Block : MonoBehaviour
 
         return true;
     }
+
 }
