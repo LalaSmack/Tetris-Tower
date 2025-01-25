8bf01a5d3d937c4f25a193c70f948b9d1b4fe785
Layal Singh <layal.singhkolkata@gmail.com>
2025-01-24 23:48:52 -0500

    Tracking highest point in stack

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+public class GridManager : MonoBehaviour
+    {
+    public static float highest = 0;
+
+    public static void HighestPointUpdate(Transform block)
+    {
+        foreach (Transform child in block)
+        {
+            if (child.position.y > highest)
+            {
+                highest = child.position.y;
+            }
+        }
+    }
+}
+
