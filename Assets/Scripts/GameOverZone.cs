a34a0bf879b0331c9aca68ff2a31954eea8885fc
Layal Singh <layal.singhkolkata@gmail.com>
2025-07-03 21:34:52 -0400

    trigger for detecting falled blocks

---
+++ Assets/Scripts/GameOverZone.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GameOverZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        Debug.Log($"Game Over: {other.gameObject.name} fell out of bounds while moving.");
+        Time.timeScale = 0; 
+
+
+    }
+    
+    
+}
+
